606ab84cebc47db8e6da8ef7d26278f3b88d0cba
Michael Sipayung <Michaelsipayung123@gmail.com>
2024-05-03 13:26:54 +0700

    fix critical bugs on doubly linked list

--- cormenalgo.h
+++ cormenalgo.h
@@ -33,20 +33,19 @@ namespace cormen {
 				--j;
 			}
 			data[j + 1] = key;
-		}	
+		}
 	}
 	template<typename T> class linked_list;
 	template<typename T>class node {
 	public:
 		node() = default;
-		explicit node(T it) : item(std::move(it)), next(nullptr){}
+		explicit node(T it) : item(std::move(it)), next(nullptr) {}
 	private:
 		T item;
 		node<T>* next;
 		friend class linked_list<T>;
 	};
 	template<typename T> class linked_list {
-		//friend std::ostream& operator<<(std::ostream&, const linked_list<T>&);
 	public:
 		linked_list();
 		~linked_list();
@@ -62,6 +61,7 @@ namespace cormen {
 	private:
 		node<T>* head;
 		size_t sz = 0;
+		node<T>* get_tail();
 	};
 
 	template<typename T>
@@ -92,11 +92,13 @@ namespace cormen {
 	void linked_list<T>::push_back(const T& item)
 	{
 		auto new_item = new node<T>(item);
-		auto temp = head;
-		while(temp->next)
-			temp = temp->next;
-		temp->next = new_item;
-		++sz;
+		auto tail = get_tail();
+		if (!tail)
+			push_front(item);
+		else {
+			tail->next = new_item;
+			++sz;
+		}
 	}
 	template<typename T>
 	void linked_list<T>::push_front(const T& item)
@@ -110,7 +112,7 @@ namespace cormen {
 	template<typename T>
 	void linked_list<T>::pop_front()
 	{
-		if(!head)
+		if (!head)
 			throw std::out_of_range("linked_list is empty before pop_front");
 		auto old = head;
 		head = old->next;
@@ -120,7 +122,7 @@ namespace cormen {
 	template<typename T>
 	void linked_list<T>::clear()
 	{
-		while (!empty()) 
+		while (!empty())
 			pop_front();
 		sz = 0;
 	}
@@ -128,16 +130,154 @@ namespace cormen {
 	void linked_list<T>::print()
 	{
 		auto temp = head;
-		while(temp)
-		{
+		while (temp){
 			cout << temp->item << " ";
 			temp = temp->next;
 		}
-		delete temp;
 	}
 	template<typename T>
-	size_t linked_list<T>::size()const{
+	size_t linked_list<T>::size()const {
 		return sz;
+	}
+	template<typename T>
+	node<T>* linked_list<T>::get_tail()
+	{
+		if (!head)
+			return nullptr;
+		auto temp = head;
+		while (temp->next)
+			temp = temp->next;
+		return temp;
+	}
+	template<typename T> class d_linked_list;
+	template<typename T> class d_node
+	{
+	public:
+		explicit d_node(const T& d = T{}, d_node<T>* p = nullptr,
+			d_node<T>* n = nullptr) : item(d), prev(p), next(n) {}
+		// for debugging purposes
+		const T& get_item()const { return item; }
+		const d_node<T>* get_prev()const { return prev; }
+		const d_node<T>* get_next()const { return next; }
+	private:
+		T item;
+		d_node<T>* prev;
+		d_node<T>* next;
+		friend class d_linked_list<T>;
+	};
+	template<typename T> class d_linked_list
+	{
+	public:
+		d_linked_list();
+		~d_linked_list();
+		[[nodiscard]] bool empty()const;
+		[[nodiscard]] const T& front()const;
+		[[nodiscard]] const T& back()const;
+		void push_front(const T&)noexcept;
+		void push_back(const T&)noexcept;
+		void pop_back();
+		void pop_front();
+		[[nodiscard]] size_t size() const;
+		const d_node<T>* get_head() const { return head; }
+		const d_node<T>* get_tail() const { return tail; }
+	private:
+		d_node<T>* head;
+		d_node<T>* tail;
+		size_t sz = 0;
+	protected:
+		void add(d_node<T>* pos, const T& val);
+		void remove(d_node<T>* pos);
+	};
+	// ctor for doubly linked list, head and tail is sentinel node for quick access
+	template<typename T>
+	d_linked_list<T>::d_linked_list()
+	{
+		head = new d_node<T>(); // create a sentinel, this is must be non null
+		tail = new d_node<T>(); // but it's only dummy node
+		head->next = tail;
+		tail->prev = head;
+	}
+	// dtor of the doubly linked list, destroy all only if the list is not empty
+	template<typename T>
+	d_linked_list<T>::~d_linked_list()
+	{
+		while (!empty())
+			pop_front(); // clear all node
+		delete head; // clear the sentinel node
+		delete tail;
+	}
+	// empty: checking the current list if contain any element except on head and tail
+	template<typename T>
+	bool d_linked_list<T>::empty() const {
+		return (head->next == tail);
+	}
+	// front: get the first element from doubly linked list
+	template<typename T>
+	const T& d_linked_list<T>::front() const {
+		if (empty())
+			throw std::out_of_range("call front on empty list");
+		return head->next->item;
+	}
+	// back: get the last element from doubly linked list
+	template<typename T>
+	const T& d_linked_list<T>::back() const
+	{
+		if (empty())
+			throw std::out_of_range("call back on empty list");
+		return tail->prev->item;
+	}
+	// push_front: adding new element on the front of the list
+	template<typename T>
+	void d_linked_list<T>::push_front(const T& val)noexcept {
+		add(head->next, val);
+		++sz;
+	}
+	// push_back: adding new element on the back of the list
+	template<typename T>
+	void d_linked_list<T>::push_back(const T& val)noexcept {
+		add(tail, val);
+		++sz;
+	}
+	// pop_back: removing the element from the bottom of the list
+	template<typename T>
+	void d_linked_list<T>::pop_back() {
+		if (empty())
+			throw std::out_of_range("call pop_back on empty list");
+		remove(tail->prev);
+		--sz;
+	}
+	// pop_front: removing the element from the top of the list
+	template<typename T>
+	void d_linked_list<T>::pop_front() {
+		if (empty())
+			throw std::out_of_range("call pop_front on empty list");
+		remove(head->next);
+		--sz;
+	}
+	// size: total element on doubly linked list, except head and tail element
+	template<typename T>
+	size_t d_linked_list<T>::size() const {
+		return sz;
+	}
+	// add: protected member of the doubly linked list class, adding item before pos
+	template<typename T>
+	void d_linked_list<T>::add(d_node<T>* pos, const T& val)
+	{
+		auto new_item = new d_node<T>(val);
+		new_item->next = pos; // link new_item in between pos
+		new_item->prev = pos->prev; // and pos->prev
+		pos->prev->next = new_item;
+		pos->prev = new_item;
+	}
+	// remove: removing node pos from list, u: predecessor, w: successor
+	template<typename T>
+	void d_linked_list<T>::remove(d_node<T>* pos)
+	{
+		auto u = pos->prev; // remove pos from list
+		auto w = pos->next; // [u -> pos ->w]
+		u->next = w;
+		w->prev = u;
+		delete pos;
 	}
 };
 #endif // !CORMEN_ALGORITHM
--- cormentalgo_test.h
+++ cormentalgo_test.h
@@ -130,4 +130,104 @@ TEST(LinkedList, sizefunction)
 	temp.clear();
 	EXPECT_EQ(0, temp.size());
 }
+TEST(LinkedList, gettail)
+{
+	auto temp = linked_list<double>();
+	temp.push_back(12.8);
+	EXPECT_EQ(12.8, temp.back());
+	EXPECT_EQ(1, temp.size());
+	temp.push_back(90.98);
+	EXPECT_EQ(90.98, temp.back());
+	EXPECT_EQ(2, temp.size());
+}
+TEST(DoublyLinkedList, popbackempty){
+	d_linked_list<double> list;
+	EXPECT_ANY_THROW(list.pop_back());
+}
+TEST(DoublyLinkedList, popfrontempty){
+	d_linked_list<int> list;
+	EXPECT_ANY_THROW(list.pop_front());
+}
+TEST(DNode, defaultbehavior) {
+	auto node = new d_node<double>(12.8);
+	EXPECT_EQ(12.8, node->get_item());
+	EXPECT_TRUE(node->get_prev() == nullptr);
+	EXPECT_TRUE(node->get_next() == nullptr);
+}
+TEST(DoublyLinkedList, defaultbehavior) {
+	d_linked_list<double> ls;
+	EXPECT_TRUE(ls.size() == 0);
+	EXPECT_ANY_THROW(ls.front());
+	EXPECT_THROW(ls.front(), std::out_of_range);
+	EXPECT_ANY_THROW(ls.back());
+	EXPECT_THROW(ls.back(), std::out_of_range);
+	EXPECT_ANY_THROW(ls.pop_back());
+	EXPECT_THROW(ls.pop_back(), std::out_of_range);
+	EXPECT_ANY_THROW(ls.pop_front());
+	EXPECT_THROW(ls.pop_front(), std::out_of_range);
+}
+TEST(DoublyLinkedList, emptylist) {
+	d_linked_list<double> ls;
+	EXPECT_TRUE(ls.empty());
+	EXPECT_TRUE(ls.get_head()!=nullptr);
+	EXPECT_TRUE(ls.get_tail()!=nullptr);
+}
+TEST(DoublyLinkedList, addingatback) {
+	d_linked_list<double> ls;
+	ls.push_back(89.9);
+	EXPECT_TRUE(!ls.empty());
+	EXPECT_TRUE(ls.size() == 1);
+}
+TEST(DoublyLinkedList, contentatback) {
+	d_linked_list<double> ls;
+	ls.push_back(89.9);
+	EXPECT_TRUE(ls.back() == 89.9);
+	ls.push_back(980.8);
+	EXPECT_TRUE(ls.back() == 980.8);
+	EXPECT_TRUE(ls.size() == 2);
+}
+TEST(DoublyLinkedList, addingatfront) {
+	d_linked_list<double> ls;
+	ls.push_front(89.9);
+	EXPECT_TRUE(!ls.empty());
+	EXPECT_TRUE(ls.size() == 1);
+}
+TEST(DoublyLinkedList, contentatfront) {
+	d_linked_list<double> ls;
+	ls.push_front(89.9);
+	EXPECT_TRUE(ls.front() == 89.9);
+	ls.push_front(980.8);
+	EXPECT_TRUE(ls.front() == 980.8);
+	EXPECT_TRUE(ls.size() == 2);
+}
+TEST(DoublyLinkedList, mixingadditem) {
+	d_linked_list<string> ls;
+	EXPECT_TRUE(ls.empty());
+	ls.push_back({ "jackie" });
+	EXPECT_FALSE(ls.empty());
+	ls.push_front({ "daniel" });
+	EXPECT_TRUE(ls.size() == 2);
+	EXPECT_TRUE(ls.back() == "jackie");
+	EXPECT_TRUE(ls.front() == "daniel");
+}
+TEST(DoublyLinkedList, sizeafterpopback) {
+	d_linked_list<string> ls;
+	ls.push_back("donald");
+	ls.push_back("dinar");
+	EXPECT_TRUE(ls.size() == 2);
+	ls.pop_back();
+	EXPECT_TRUE(ls.size() == 1);
+	ls.pop_back();
+	EXPECT_TRUE(ls.size() == 0);
+}
+TEST(DoublyLinkedList, sizeafterpopfront) {
+	d_linked_list<string> ls;
+	ls.push_front("donald");
+	ls.push_front("dinar");
+	EXPECT_TRUE(ls.size() == 2);
+	ls.pop_front();
+	EXPECT_TRUE(ls.size() == 1);
+	ls.pop_front();
+	EXPECT_TRUE(ls.size() == 0);
+}
 #endif
